d7cf8ce880487e732827c3e64720463928433190
lastahmedabbasi <ahmedabbasiyt@gmail.com>
2026-09-04 01:36:02 +0500

    Create task3.c

---
+++ task3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int age;
+    float height;
+    char grade;
+
+    printf("Enter age: ");
+    scanf("%d", &age);
+
+    printf("Enter height: ");
+    scanf("%f", &height);
+
+    printf("Enter grade: ");
+    scanf("\n%c", &grade);
+
+    printf("\nAge: %d\n", age);
+    printf("Height: %f\n", height);
+    printf("Grade: %c", grade);
+
+    return 0;
+}
